4491a69fe0f7f945fcabe9048dc3f90260d174c9
CJ Fallorina <chescafallorina0@gmail.com>
2024-07-02 06:41:21 -0700

    modified:   LaundrySystem/BusinessClient_Registration/LaundrySQL.sql

--- LaundrySystem/BusinessClient_Registration/LaundrySQL.sql
+++ LaundrySystem/BusinessClient_Registration/LaundrySQL.sql
@@ -7,19 +7,24 @@
  * Created: Jun 29, 2024
  */
 
---Database for Laundry System
+-- Database for Laundry System
 
---Create db if not exist
-CREATE DATABASE IF NOT EXIST dba_laundry
+-- Create db if not exist
+CREATE DATABASE IF NOT EXISTS dba_laundry;
 
---Use the created database for user/client request
+-- Use the created database for user/client request
 USE dba_laundry;
 
---Request Section
 
---Client/User Section
 
---Create User/Client Request Table
+
+
+
+-- Request Section
+
+-- Client/User Section
+
+-- Create User/Client Request Table
 CREATE TABLE IF NOT EXISTS request_user (
     rquser_ID INT PRIMARY KEY,
     rquser_name VARCHAR(225) NOT NULL,
@@ -35,22 +40,20 @@ CREATE TABLE IF NOT EXISTS request_user (
     FOREIGN KEY (rquser_ID) REFERENCES request_userpic(rquserpic_ID) ON DELETE CASCADE
 );
 
---Create User/Client Profile table
+-- Create User/Client Profile table
 CREATE TABLE IF NOT EXISTS request_userpic (
     rquserpic_ID INT AUTO_INCREMENT PRIMARY KEY,
     rquser_name VARCHAR(225) NOT NULL,
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
-    data LONGBLOB  NOT NULL
+    data LONGBLOB NOT NULL
 );
 
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
---===========================
+-- Employee Section
 
-
---Employee Section
-
---Create Employee Request Table
+-- Create Employee Request Table
 CREATE TABLE IF NOT EXISTS request_employee(
     rqemp_ID INT PRIMARY KEY,
     rqemp_name VARCHAR(225) NOT NULL,
@@ -60,14 +63,14 @@ CREATE TABLE IF NOT EXISTS request_employee(
     rqemp_add VARCHAR(225) NOT NULL,
     rqemp_city CHAR(35) NOT NULL,
     rqemp_brgy CHAR(35) NOT NULL,
-    rq_shop  VARCHAR(225) NOT NULL,
+    rq_shop VARCHAR(225) NOT NULL,
     rq_date DATE NOT NULL,
     rqemp_username VARCHAR(225) NOT NULL,
     rqemp_userpass VARCHAR(225) NOT NULL,
     rqemp_status CHAR(15) DEFAULT 'Pending'
 );
 
---Create Employee Request Profile Table
+-- Create Employee Request Profile Table
 CREATE TABLE IF NOT EXISTS request_employeepic (
     rqemppic_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqemp_name VARCHAR(225) NOT NULL,
@@ -76,40 +79,38 @@ CREATE TABLE IF NOT EXISTS request_employeepic (
     data LONGBLOB NOT NULL
 );
 
---Create Employee Request SSS File table
+-- Create Employee Request SSS File table
 CREATE TABLE IF NOT EXISTS request_empSSS (
     rqempSSS_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqempSSS_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Employee Request  PhilHealth File table
+-- Create Employee Request PhilHealth File table
 CREATE TABLE IF NOT EXISTS request_empPhil (
     rqempPhil_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqempPhil_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Employee Request Pag-IBIG File table
+-- Create Employee Request Pag-IBIG File table
 CREATE TABLE IF NOT EXISTS request_empPB (
     rqempPB_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqempPB_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
---===========================
+-- Business Section
 
-
---Business Section
-
---Create Business Request table
+-- Create Business Request table
 CREATE TABLE IF NOT EXISTS request_business (
     rqbs_ID INT PRIMARY KEY,
     rqbs_name VARCHAR(225) NOT NULL,
@@ -119,13 +120,13 @@ CREATE TABLE IF NOT EXISTS request_business (
     rqbs_brgy CHAR(35) NOT NULL,
     rqbs_contact VARCHAR(15) NOT NULL,
     rqbs_email VARCHAR(85) NOT NULL,
-    rqbs_regdate DATA NOT NULL,
-    rqbs-username VARCHAR(225) NOT NULL,
+    rqbs_regdate DATE NOT NULL,
+    rqbs_username VARCHAR(225) NOT NULL,
     rqbs_userpass VARCHAR(225) NOT NULL,
-    rqbs_status CHAR(15) DEFAULT 'Pending',
-)
+    rqbs_status CHAR(15) DEFAULT 'Pending'
+);
 
---Create Business Owner Profile Request table
+-- Create Business Owner Profile Request table
 CREATE TABLE IF NOT EXISTS request_bsOwner (
     rqbs_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqbs_owner VARCHAR(225) NOT NULL,
@@ -133,36 +134,36 @@ CREATE TABLE IF NOT EXISTS request_bsOwner (
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business Logo Request table
+-- Create Business Logo Request table
 CREATE TABLE IF NOT EXISTS request_bsLogo (
     rqbs_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqbslogo_name VARCHAR(225) NOT NULL,
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business DTI File Request table
+-- Create Business DTI File Request table
 CREATE TABLE IF NOT EXISTS request_bsDTI (
     rqbsDTI_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqbsDTI_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business TIN File Request table
+-- Create Business TIN File Request table
 CREATE TABLE IF NOT EXISTS request_bsTIN (
     rqbsTIN_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqbsTIN_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business Agreement Request table
+-- Create Business Agreement Request table
 CREATE TABLE IF NOT EXISTS request_bsAgreement (
     rqbs_ID INT AUTO_INCREMENT PRIMARY KEY,
     rqbs_owner VARCHAR(225) NOT NULL,
@@ -170,26 +171,15 @@ CREATE TABLE IF NOT EXISTS request_bsAgreement (
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+-- Registered Section
 
+-- Client/User Section
 
-
-
---++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
-
-
---Registered Section
-
-
-
---Client/User Section
-
---Create Client User table
+-- Create Client User table
 CREATE TABLE IF NOT EXISTS user_info (
     user_ID INT PRIMARY KEY,
     user_name VARCHAR(225) NOT NULL,
@@ -202,24 +192,24 @@ CREATE TABLE IF NOT EXISTS user_info (
     user_username VARCHAR(225) NOT NULL,
     user_userpass VARCHAR(225) NOT NULL,
     rquser_status CHAR(15) DEFAULT 'Approved',
-    FOREIGN KEY (rquser_ID) REFERENCES request_userpic(rquserpic_ID) ON DELETE CASCADE
-)
+    FOREIGN KEY (user_ID) REFERENCES request_userpic(rquserpic_ID) ON DELETE CASCADE
+);
 
---Create Client/User table
-CREATE TABLE IF NOT EXIST user_profile (
+-- Create Client/User Profile table
+CREATE TABLE IF NOT EXISTS user_profile (
     userpic_ID INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(225) NOT NULL,
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
-    data LONGBLOB  NOT NULL
-)
+    data LONGBLOB NOT NULL
+);
 
---===========================
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
---Employee Section
+-- Employee Section
 
---Create Employee table
-CREATE IF NOT EXISTS employee (
+-- Create Employee table
+CREATE TABLE IF NOT EXISTS employee (
     emp_ID INT PRIMARY KEY,
     emp_name VARCHAR(225) NOT NULL,
     emp_bday DATE NOT NULL,
@@ -228,15 +218,15 @@ CREATE IF NOT EXISTS employee (
     emp_add VARCHAR(225) NOT NULL,
     emp_city CHAR(35) NOT NULL,
     emp_brgy CHAR(35) NOT NULL,
-    work_shop  VARCHAR(225) NOT NULL,
+    work_shop VARCHAR(225) NOT NULL,
     rg_date DATE NOT NULL,
     emp_username VARCHAR(225) NOT NULL,
     emp_userpass VARCHAR(225) NOT NULL,
     emp_status CHAR(15) DEFAULT 'Approved'
-)
+);
 
---Create Employee Profile table 
-CREATE IF NOT EXISTS employee_profile (
+-- Create Employee Profile table 
+CREATE TABLE IF NOT EXISTS employee_profile (
     emppic_ID INT AUTO_INCREMENT PRIMARY KEY,
     emp_name VARCHAR(225) NOT NULL,
     mime VARCHAR(50) NOT NULL,
@@ -244,41 +234,38 @@ CREATE IF NOT EXISTS employee_profile (
     data LONGBLOB NOT NULL
 );
 
---Create Employee SSS File table
+-- Create Employee SSS File table
 CREATE TABLE IF NOT EXISTS employee_SSS (
     empSSS_ID INT AUTO_INCREMENT PRIMARY KEY,
     empSSS_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Employee PhilHealth File table
+-- Create Employee PhilHealth File table
 CREATE TABLE IF NOT EXISTS employee_Phil (
     empPhil_ID INT AUTO_INCREMENT PRIMARY KEY,
     empPhil_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Employee Pag-IBIG File table
+-- Create Employee Pag-IBIG File table
 CREATE TABLE IF NOT EXISTS employee_PB (
     empPB_ID INT AUTO_INCREMENT PRIMARY KEY,
     empPB_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+-- Business Section
 
---===========================
-
-
---Business Section
-
---Create Business Name Registered Table 
+-- Create Business Name Registered Table 
 CREATE TABLE IF NOT EXISTS laundry_shops (
     bs_ID INT AUTO_INCREMENT PRIMARY KEY,
     bs_name VARCHAR(225) NOT NULL,
@@ -288,23 +275,18 @@ CREATE TABLE IF NOT EXISTS laundry_shops (
     bs_brgy CHAR(35) NOT NULL,
     bs_contact VARCHAR(15) NOT NULL,
     bs_email VARCHAR(85) NOT NULL,
-    bs_regdate DATA NOT NULL
-    bs_status CHAR(15) DEFAULT 'Approved',
-)
+    bs_regdate DATE NOT NULL,
+    bs_status CHAR(15) DEFAULT 'Approved'
+);
 
---bs_status after official registration:
---"Approved" - only once the business is registered
---"Unpaid" - Passed due_pay (see pending_payment table)
---"Paid" - once due is paid)
-
---Create Business Account Info
+-- Create Business Account Info
 CREATE TABLE IF NOT EXISTS laundry_account(
     bs_ID INT AUTO_INCREMENT PRIMARY KEY,
     bs_username VARCHAR(225) NOT NULL,
     bs_userpass VARCHAR(225) NOT NULL
-)
+);
 
---Create Owner Info
+-- Create Owner Info
 CREATE TABLE IF NOT EXISTS business_owner (
     bs_ID INT AUTO_INCREMENT PRIMARY KEY,
     bs_owner VARCHAR(225) NOT NULL,
@@ -312,18 +294,18 @@ CREATE TABLE IF NOT EXISTS business_owner (
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business Logo table
+-- Create Business Logo table
 CREATE TABLE IF NOT EXISTS laundry_Logo (
     bs_ID INT AUTO_INCREMENT PRIMARY KEY,
     bslogo_name VARCHAR(225) NOT NULL,
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business DTI File table
+-- Create Business DTI File table
 CREATE TABLE IF NOT EXISTS businessDTI_File (
     bs_ID INT AUTO_INCREMENT PRIMARY KEY,
     bsDTI_ID INT NOT NULL,
@@ -331,19 +313,19 @@ CREATE TABLE IF NOT EXISTS businessDTI_File (
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business TIN File table
-CREATE TABLE IF NOT EXISTS rbusinessTIN_File (
+-- Create Business TIN File table
+CREATE TABLE IF NOT EXISTS businessTIN_File (
     bs_ID INT AUTO_INCREMENT PRIMARY KEY,
     bsTIN_ID INT NOT NULL,
     bsTIN_name VARCHAR(225) NOT NULL,
     mime VARCHAR(100) NOT NULL,
     size INT NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
---Create Business Agreement table
+-- Create Business Agreement table
 CREATE TABLE IF NOT EXISTS business_Agreements (
     bs_ID INT AUTO_INCREMENT PRIMARY KEY,
     bs_owner VARCHAR(225) NOT NULL,
@@ -351,36 +333,45 @@ CREATE TABLE IF NOT EXISTS business_Agreements (
     mime VARCHAR(50) NOT NULL,
     size BIGINT(20) NOT NULL,
     data LONGBLOB NOT NULL
-)
+);
 
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
---===========================
+-- Subscription Section 
 
-
---Subscription Section 
-
---Create Price table
+-- Create Price table
 CREATE TABLE IF NOT EXISTS subscription (
     sub_ID CHAR(5) PRIMARY KEY,
     sub_name CHAR(15) NOT NULL,
     sub_price INT NOT NULL,
     sub_pay CHAR(15) NOT NULL
-)
+);
 
---Create Pending Payment Table
-CREATE IF NOT EXISTS pending_payment (
+-- Create Pending Payment Table
+CREATE TABLE IF NOT EXISTS pending_payment (
     curr_ID INT AUTO_INCREMENT PRIMARY KEY,
     bs_ID INT NOT NULL,
     sub_ID CHAR(5),
     total_pay INT NOT NULL,
     due_pay DATE NOT NULL
-)
+);
 
---Create Payment table
+-- Create Proof Payment table (Screenshot of payment since API payment gateway is literally killing me)
+CREATE TABLE IF NOT EXISTS proof_payment (
+    paypic_ID INT AUTO_INCREMENT PRIMARY KEY,
+    bs_name VARCHAR(225) NOT NULL,
+    paypic_name VARCHAR(225) NOT NULL,
+    mime VARCHAR(50) NOT NULL,
+    size BIGINT(20) NOT NULL,
+    data LONGBLOB NOT NULL
+);
+
+-- Create Payment table
 CREATE TABLE IF NOT EXISTS payment (
     pay_ID INT AUTO_INCREMENT PRIMARY KEY,
     bs_ID INT NOT NULL,
     sub_ID CHAR(5) NOT NULL,
     total_pay INT NOT NULL,
-    pay_date DATE NOT NULL
-)
+    pay_date DATE NOT NULL,
+    paypic_ID INT NOT NULL
+);
