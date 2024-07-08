430a2be3677ee3f9597033ba662a35d1b7726c5e
@Jeem11 <jhonmarkmabandos1@gmail.com>
2024-07-09 04:55:24 +0800

    antok per di susuko

--- LaundrySystem/Edashboard/Edashboard.php
+++ LaundrySystem/Edashboard/Edashboard.php
@@ -7,7 +7,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Admin Dashboard</title>
+    <title>Business Employee Dashboard</title>
 
     <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
 
--- LaundrySystem/Owner_Dashboard/OwnerPanel.php
+++ LaundrySystem/Owner_Dashboard/OwnerPanel.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Admin Dashboard</title>
+    <title>Business Owner Dashboard</title>
 
     <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
 
